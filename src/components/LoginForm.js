1f466c4a9f6b8afd3c675daccb42add99b4a1ae9
Kyle Coberly <kyle.coberly@gmail.com>
2019-06-20 10:44:40 -0600

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,40 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+  updateField = event => {
+    this.setState(state => {
+      state[event.target.name] = event.target.value
+      return state
+    })
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    if (this.state.username && this.state.password){
+      this.props.handleLogin(event)
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={this.updateField} id="username" name="username" type="text" value={this.state.username} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={this.updateField} id="password" name="password" type="password" value={this.state.password} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,26 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+  updateMessage = event => {
+    this.setState(state => {
+      state.message = event.target.value
+      return state
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={this.updateMessage} type="text" name="message" id="message" value={this.state.message} />
+        <p>{this.props.maxChars - this.state.message.length}</p>
       </div>
     );
   }
